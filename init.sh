41ec5b896803c8bde5b30943f80c0774f774efe1
Vectormaster <vectormaster123456789@gmail.com>
2023-01-04 11:26:05 +0200

    fix: add genesis accounts

--- init.sh
+++ init.sh
@@ -1,24 +1,49 @@
+#!/bin/bash
+
 # sudo apt update
 # sudo apt upgrade -y
 # sudo apt install build-essential jq -y
 
+DENOM="token"
+CHAIN_ID="lottery-12345_1"
+KEYRING_BACKEND="test"
+# BLOCK_TIME="5m0s"
+BLOCK_TIME="3s"
+MONIKER="localtestnet"
+CLIENT_NUM=2
+INITIAL_TOKEN_AMOUNT=500
+
+
 sudo rm -rf ~/.lottery
 make install
 
-lotteryd init localtestnet --chain-id lottery-12345_1 
+lotteryd init $MONIKER --chain-id $CHAIN_ID
 
-echo "claim either tribe mercy genre drastic stamp spring attend ready believe material hedgehog space remind valley give slight cram arm release universe hybrid abuse" | lotteryd keys add validator1 --keyring-backend test --recover
+lotteryd config keyring-backend $KEYRING_BACKEND
 
-lotteryd add-genesis-account $(lotteryd keys show validator1 -a --keyring-backend test) 1000000000token 
+# Change parameter token denominations to token
+cat $HOME/.lottery/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="'$DENOM'"' > $HOME/.lottery/config/tmp_genesis.json && mv $HOME/.lottery/config/tmp_genesis.json $HOME/.lottery/config/genesis.json
+cat $HOME/.lottery/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="'$DENOM'"' > $HOME/.lottery/config/tmp_genesis.json && mv $HOME/.lottery/config/tmp_genesis.json $HOME/.lottery/config/genesis.json
+cat $HOME/.lottery/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="'$DENOM'"' > $HOME/.lottery/config/tmp_genesis.json && mv $HOME/.lottery/config/tmp_genesis.json $HOME/.lottery/config/genesis.json
+cat $HOME/.lottery/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="'$DENOM'"' > $HOME/.lottery/config/tmp_genesis.json && mv $HOME/.lottery/config/tmp_genesis.json $HOME/.lottery/config/genesis.json
+cat $HOME/.lottery/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="'$DENOM'"'> $HOME/.lottery/config/tmp_genesis.json && mv $HOME/.lottery/config/tmp_genesis.json $HOME/.lottery/config/genesis.json
 
-echo "require resist steak energy armed prison embody abuse huge submit host subway merit kiwi inherit distance cliff suffer general program connect link employ crew" | lotteryd keys add account1 --keyring-backend test --recover
+# Change block time (5min per block)
+sed -i 's/timeout_commit = "5s"/timeout_commit = "'${BLOCK_TIME}'"/g' $HOME/.lottery/config/config.toml
 
-lotteryd add-genesis-account $(lotteryd keys show account1 -a --keyring-backend test) 200000token
+lotteryd keys add validator
 
-lotteryd gentx validator1 50000000token --keyring-backend test --chain-id lottery-12345_1
+lotteryd add-genesis-account $(lotteryd keys show validator -a) 1000000000token
+
+for (( i=1; i<=CLIENT_NUM; i++ ))
+do
+    echo "Creating Client $i"
+    lotteryd keys add "client$i" 
+    lotteryd add-genesis-account $(lotteryd keys show "client$i" -a ) ${i}${DENOM}
+done
+
+lotteryd gentx validator 50000000token --chain-id $CHAIN_ID
 
 lotteryd collect-gentxs
 
-sed -i 's/stake/token/g' ~/.lottery/config/genesis.json
-
-lotteryd start
+lotteryd start --pruning=nothing --log_level info
